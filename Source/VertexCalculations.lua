7b6ca376ab577347e6a1c91f6d030b9b71990fd3
Teelo <49792789+teelolws@users.noreply.github.com>
2023-08-20 12:18:16 +1200

    Improvements to ring prediction algorithm

--- Source/VertexCalculations.lua
+++ Source/VertexCalculations.lua
@@ -6,7 +6,7 @@ addon.currentVerticesTimes = {}
 
 local MIN_VERTICES = 4
 local MAX_VERTICES = 10
-local CHANGE_THRESHOLD = 30
+local CHANGE_THRESHOLD = math.pi/8
 local OVERRIDE_VERTICES = 6
 local SPACING = 3
 local SWITCH_DIRECTION_THRESHOLD = 4
@@ -26,43 +26,45 @@ function addon.findVertices(currentQuest)
     repeat
         wipe(addon.currentVertices)
         
-        local lastX, lastY = data.nodes[1].x, data.nodes[1].y
-        local turningPositiveX, turningPositiveY = (data.nodes[2].x - data.nodes[1].x) >= 0, (data.nodes[2].y - data.nodes[1].y) >= 0
-        lastTime = data.nodes[1].time
-        
-        for i = 2, #data.nodes do
-            local node = data.nodes[i]
-            local deltaX, deltaY = node.x - lastX, node.y - lastY
-            
-            if ((node.time - lastTime > SPACING)) and ((deltaX > threshold) or (deltaX < (-1 * threshold)) and ((deltaY > threshold) or (deltaY < (-1 * threshold)))) then
-                lastX, lastY, lastTime = node.x, node.y, node.time
-                
-                local earlierNode = data.nodes[i-5]
-                if i < 5 then
-                    earlierNode = node
-                end
-                table.insert(addon.currentVertices, earlierNode)
-            
-            else
-                if turningPositiveX and (deltaX < (-1 * SWITCH_DIRECTION_THRESHOLD)) then
-                    lastX = node.x
-                    turningPositiveX = false
-                elseif (not turningPositiveX) and (deltaX > SWITCH_DIRECTION_THRESHOLD) then
-                    lastX = node.x
-                    turningPositiveX = true
-                end
-                
-                if turningPositiveY and (deltaY < (-1 * SWITCH_DIRECTION_THRESHOLD)) then
-                    lastY = node.y
-                    turningPositiveY = false
-                elseif (not turningPositiveY) and (deltaY > SWITCH_DIRECTION_THRESHOLD) then
-                    lastY = node.y
-                    turningPositiveY = true
-                end
-            end
+        local originX, originY = data.nodes[1].x, data.nodes[1].y
+        local lastX, lastY = data.nodes[2].x - originX, data.nodes[2].y - originY
+        if (lastX == 0) and (lastY == 0) then
+            print("Speedrun: divide by zero error", lastX, lastY)
+            return
         end
         
-        threshold = threshold + 5
+        local magLast = math.sqrt((lastX*lastX) + (lastY*lastY))
+        local lastTime = data.nodes[2].time
+        -- using dotproduct formula:
+        -- angle = cos^-1 [ a . b / |a| |b| ]
+                
+        for i = 3, #data.nodes do
+            local node = data.nodes[i]
+            local nodeX, nodeY = node.x - originX, node.y - originY
+            
+            if (nodeX == 0) and (nodeY == 0) then
+                print("Speedrun: divide by zero error", nodeX, nodeY, i)
+                return
+            end
+            
+            local nodeMag = math.sqrt((nodeX*nodeX) + (nodeY*nodeY))
+            
+            local dotproduct = (lastX * nodeX) + (lastY * nodeY)
+            
+            local angle = math.acos(dotproduct / (magLast * nodeMag))
+            
+            if ((node.time - lastTime > SPACING)) and ((angle > threshold) or (angle < (threshold*-1))) then
+                table.insert(addon.currentVertices, node)
+                
+                originX, originY = data.nodes[i-1].x, data.nodes[i-1].y
+                lastX, lastY = node.x - originX, node.y - originY
+                magLast = math.sqrt((lastX*lastX) + (lastY*lastY))
+                lastTime = node.time
+            end
+
+        end
+        
+        threshold = threshold + 0.001
     until (#addon.currentVertices < MAX_VERTICES)
     
     table.insert(addon.currentVertices, data.nodes[#data.nodes])
